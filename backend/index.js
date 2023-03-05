5482b36c201824e57ef87c30bbc7a0c567f2acd7
Abhishek Roy <abhishekroy8487@gmail.com>
2023-03-05 23:21:50 +0530

    initial project setup

---
+++ backend/index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
