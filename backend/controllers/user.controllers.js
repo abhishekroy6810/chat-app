5df4a2f032533a6efc1c156afd11b4690e077243
Abhishek Roy <abhishekroy8487@gmail.com>
2023-03-11 14:35:58 +0530

    token authentication added

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,5 +1,6 @@
 const expressAsyncHandler = require("express-async-handler");
 const USER = require("../database/models/user.model");
+const generateToken = require("../utils/generateToken");
 
 const registerUser = expressAsyncHandler(async (req, res) => {
   const { name, email, password, photo } = req.body;
@@ -25,6 +26,7 @@ const registerUser = expressAsyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       phtoto: user.photo,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400).json("User Not Found");
@@ -43,6 +45,7 @@ const loginUser = expressAsyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       photo: user.photp,
+      token: generateToken(user._id),
     });
   } else {
     res.status(401).json("Invalid Email Or Password");
