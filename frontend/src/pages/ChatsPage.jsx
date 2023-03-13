f128492ab4b5f46b2a13445628c89fb449b8a0c8
Abhishek Roy <abhishekroy8487@gmail.com>
2023-03-13 23:21:12 +0530

    chat page structure done

--- frontend/src/pages/ChatsPage.jsx
+++ frontend/src/pages/ChatsPage.jsx
@@ -1,10 +1,35 @@
+import { Box } from "@chakra-ui/react";
 import React from "react";
 
 const ChatsPage = () => {
   return (
-    <div>
-      <p>ChatsPage</p>
-    </div>
+    <Box
+      width="100%"
+      height="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      bg="black"
+    >
+      <Box
+        width={{ base: "100%", md: "95%" }}
+        height={{ base: "100%", md: "95%" }}
+        bg="white"
+        borderRadius="lg"
+        boxShadow="lg"
+        borderWidth="3px"
+        p={3}
+      >
+        header
+        <Box
+          display="flex"
+          justifyContent="space-between"
+        >
+          <span>my chats</span>
+          <span>chat box</span>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
